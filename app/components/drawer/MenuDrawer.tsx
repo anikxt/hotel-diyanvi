8338c9127fc4f277d419987b1226fba9fa955a65
Aniket Kanere <aniket15304@gmail.com>
2025-08-30 20:04:01 +0530

    feat: Implement Responsive Menu

--- app/components/drawer/MenuDrawer.tsx
+++ app/components/drawer/MenuDrawer.tsx
@@ -24,14 +24,23 @@ function MenuDrawer({ isOpen, onClose }: MenuDrawerProps) {
 
       {/* Drawer */}
       <div
-        className={`fixed top-0 -left-1 h-screen w-[520px] bg-quaternary z-50 transform transition-transform duration-300 ease-in-out ${
+        className={`fixed top-0 -left-1 h-screen sm:w-[450px] md:w-[520px] bg-quaternary z-50 transform transition-transform duration-300 ease-in-out ${
           isOpen ? 'translate-x-0' : '-translate-x-full'
         }`}
       >
-        <div className="flex justify-between items-center h-17 px-8 pt-2 bg-white shadow-[0_4px_6px_-1px_rgba(0,0,0,0.075)]">
-          <h3 className="text-3xl font-pangaia-medium text-quaternary">
-            Diyanvi
-          </h3>
+        <div className="flex justify-between items-center h-16 2xl:h-17 px-8 pt-2 bg-white shadow-[0_4px_6px_-1px_rgba(0,0,0,0.075)]">
+          <div className="flex items-center gap-3">
+            <Image
+              src="/images/common icon/logo.png"
+              alt="Diyanvi"
+              width={100}
+              height={100}
+              className="sm:hidden w-[36px] h-[36px]"
+            />
+            <h3 className="text-2xl sm:text-3xl font-pangaia-medium text-quaternary">
+              Diyanvi Menu
+            </h3>
+          </div>
           <button
             onClick={onClose}
             className="cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-colors mb-0.5"
@@ -45,9 +54,9 @@ function MenuDrawer({ isOpen, onClose }: MenuDrawerProps) {
           </button>
         </div>
 
-        <div className="px-8 mt-7">
-          <div className="flex flex-col gap-3.5 px-2">
-            <h4 className="text-menu-1 font-roboto text-xl font-medium tracking-normal">
+        <div className="px-8 mt-6 2xl:mt-7">
+          <div className="flex flex-col gap-2 2xl:gap-3.5 px-2">
+            <h4 className="text-menu-1 font-roboto text-lg sm:text-xl font-medium tracking-normal">
               Hotel
             </h4>
             <div className="flex items-center gap-6">
@@ -57,7 +66,7 @@ function MenuDrawer({ isOpen, onClose }: MenuDrawerProps) {
                 width={28}
                 height={28}
               />
-              <p className="text-primary pt-2 font-editorial-old-regular text-xl font-medium">
+              <p className="text-primary pt-2 font-editorial-old-regular text-lg sm:text-xl font-medium">
                 Rooms & Suites
               </p>
             </div>
@@ -68,7 +77,7 @@ function MenuDrawer({ isOpen, onClose }: MenuDrawerProps) {
                 width={28}
                 height={28}
               />
-              <p className="text-primary pt-2 font-editorial-old-regular text-xl font-medium">
+              <p className="text-primary pt-2 font-editorial-old-regular text-lg sm:text-xl font-medium">
                 Restaurant
               </p>
             </div>
@@ -79,39 +88,40 @@ function MenuDrawer({ isOpen, onClose }: MenuDrawerProps) {
                 width={28}
                 height={28}
               />
-              <p className="text-primary pt-2 font-editorial-old-regular text-xl font-medium">
+              <p className="text-primary pt-2 font-editorial-old-regular text-lg sm:text-xl font-medium">
                 Rooftop Restro
               </p>
             </div>
           </div>
 
-          <div className="border-b-2 border-menu-1 mt-8"></div>
+          <div className="border-b-2 border-menu-1 mt-4 2xl:mt-8"></div>
         </div>
 
-        <div className="px-8 mt-7 flex flex-col items-center gap-4">
-          <div className="flex items-center gap-4">
-            <p className="text-menu-1 font-roboto text-xl font-medium tracking-normal">
+        <div className="px-8 mt-6 flex flex-col items-start gap-1 2xl:gap-4">
+          <div className="flex flex-col sm:flex-row items-center gap-2 sm:gap-4">
+            <p className="text-menu-1 font-roboto text-base sm:text-lg 2xl:text-xl font-medium tracking-normal">
               Call us For Reservation
             </p>
-            <div className="flex items-center gap-1 bg-menu-1 px-2 py-3 pr-5 rounded-lg">
+            <div className="flex items-center gap-1 bg-menu-1 px-2 py-3 pr-5 rounded-lg justify-start sm:justify-center">
               <Image
                 src="/images/menu/image-4.png"
                 alt="Diyanvi"
                 width={40}
                 height={40}
+                className="w-[30px] h-[30px] 2xl:w-[40px] 2xl:h-[40px]"
               />
-              <p className="text-menu-1 font-roboto text-lg font-medium tracking-normal">
+              <p className="text-menu-1 font-roboto text-sm sm:text-base 2xl:text-lg font-medium tracking-normal">
                 +91 8378 83982
               </p>
             </div>
           </div>
 
-          <div className="border-b-2 border-menu-1 mt-3 w-full"></div>
+          <div className="border-b-2 border-menu-1 mt-4 w-full"></div>
         </div>
 
         {/* Location */}
-        <div className="px-8 mt-8 flex flex-col items-start justify-center gap-3">
-          <p className="text-menu-1 font-roboto text-xl font-medium tracking-normal">
+        <div className="px-8 mt-5 2xl:mt-8 flex flex-col items-start justify-center gap-2 sm:gap-3">
+          <p className="text-menu-1 font-roboto text-lg sm:text-xl font-medium tracking-normal">
             Location
           </p>
           <Image
@@ -119,12 +129,12 @@ function MenuDrawer({ isOpen, onClose }: MenuDrawerProps) {
             alt="Diyanvi"
             width={420}
             height={180}
-            className="pl-2 rounded h-[200px] w-[440px] object-cover"
+            className="pl-2 rounded h-[180px] w-[360px] 2xl:h-[200px] 2xl:w-[440px] object-cover"
           />
-          <p className="text-menu-1 font-roboto text-xl font-medium tracking-normal mt-4">
+          <p className="text-menu-1 font-roboto text-lg sm:text-xl font-medium tracking-normal mt-4 2xl:mt-4">
             Address
           </p>
-          <p className="text-primary font-roboto text-base font-medium tracking-wide max-w-[48ch]">
+          <p className="text-primary font-roboto text-sm sm:text-base font-medium leading-snug tracking-wide max-w-[48ch]">
             225 - P, Post, Hebbal Industrial Estate, Hebbal Industrial Area,
             Belawadi, Mysuru, Karnataka 570018
           </p>
